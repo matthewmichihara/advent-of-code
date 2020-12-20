1b65c997e77dc820be55b0c876e5f7f21005b223
Matthew Michihara <michihara@google.com>
2020-12-20 15:43:05 -0800

    gross

--- 2020/20/2.go
+++ 2020/20/2.go
@@ -37,13 +37,98 @@ func main() {
 		row := make([]Tile, size)
 		grid[i] = row
 	}
-	place(tiles, grid, 0, 0, size)
+	solution := place(tiles, grid, 0, 0, size)
+
+	// Part 1
+	fmt.Printf("1: %v\n",
+		solution[0][0].id*
+			solution[0][size-1].id*
+			solution[size-1][0].id*
+			solution[size-1][size-1].id,
+	)
+
+	trimmedSolution := make([][]Tile, len(solution))
+	for r, row := range solution {
+		trimmedSolution[r] = make([]Tile, len(row))
+		for c, tile := range row {
+			trimmedSolution[r][c] = tile.trim()
+		}
+	}
+	trimmedImage := makeImage(trimmedSolution)
+
+	numMonsters := 0
+	fakeTile := Tile{0, trimmedImage}
+	for _, tile := range variants(fakeTile) {
+		// Take every monster grid area, and see if a monster is there
+		for r, row := range tile.contents[:len(tile.contents)-3] {
+			for c := range row[:len(row)-20] {
+				area := make([][]rune, 3)
+				for i := 0; i < 3; i++ {
+					area[i] = make([]rune, 20)
+					copy(area[i], tile.contents[r+i][c:c+20])
+				}
+				if hasMonster(area) {
+					numMonsters++
+				}
+			}
+		}
+		if numMonsters > 0 {
+			break
+		}
+	}
+
+	monsterSize := 15
+	numWaves := 0
+	for _, row := range trimmedImage {
+		for _, item := range row {
+			if item == '#' {
+				numWaves++
+			}
+		}
+	}
+
+	fmt.Printf("2: %v\n", numWaves-(numMonsters*monsterSize))
 }
 
-func place(tiles []Tile, grid [][]Tile, r int, c int, size int) bool {
+func hasMonster(area [][]rune) bool {
+	monster := [][]rune{
+		[]rune("..................O."),
+		[]rune("O....OO....OO....OOO"),
+		[]rune(".O..O..O..O..O..O..."),
+	}
+
+	for r, row := range area {
+		for c, item := range row {
+			if monster[r][c] == 'O' && item != '#' {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func makeImage(grid [][]Tile) [][]rune {
+	tileLength := len(grid[0][0].contents)
+	image := make([][]rune, 0)
+
+	for _, tileRow := range grid {
+		for rowIndex := 0; rowIndex < tileLength; rowIndex++ {
+			imageRow := make([]rune, 0)
+			for _, tile := range tileRow {
+				row := tile.contents[rowIndex]
+				imageRow = append(imageRow, row...)
+			}
+			image = append(image, imageRow)
+		}
+	}
+
+	return image
+}
+
+func place(tiles []Tile, grid [][]Tile, r int, c int, size int) [][]Tile {
 	if len(tiles) == 0 {
-		fmt.Println(grid[0][0].id * grid[0][size-1].id * grid[size-1][0].id * grid[size-1][size-1].id)
-		return true
+		return grid
 	}
 
 	nextR, nextC := r, c+1
@@ -80,13 +165,14 @@ func place(tiles []Tile, grid [][]Tile, r int, c int, size int) bool {
 			copy(newTiles, tiles)
 			newTiles = append(newTiles[:tileIndex], newTiles[tileIndex+1:]...)
 
-			if place(newTiles, newGrid, nextR, nextC, size) {
-				return true
+			solution := place(newTiles, newGrid, nextR, nextC, size)
+			if len(solution) != 0 {
+				return solution
 			}
 		}
 	}
 
-	return false
+	return [][]Tile{}
 }
 
 func (tile Tile) top() string {
@@ -111,6 +197,16 @@ func (tile Tile) right() string {
 		ret[i] = row[len(row)-1]
 	}
 	return string(ret)
+}
+
+func (tile Tile) trim() Tile {
+	newContents := make([][]rune, len(tile.contents)-2)
+	for i, row := range tile.contents[1 : len(tile.contents)-1] {
+		newRow := make([]rune, len(row)-2)
+		copy(newRow, row[1:len(row)-1])
+		newContents[i] = newRow
+	}
+	return Tile{tile.id, newContents}
 }
 
 func variants(tile Tile) []Tile {
